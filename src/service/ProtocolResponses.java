b1583deffffaa5ed7e8293fa3e525cb64d279854
Wouter Lueks <wouter@telox.net>
2012-08-15 13:14:10 +0200

    Make cooperation with library easier

---
+++ src/service/ProtocolResponses.java
@@ -0,0 +1,19 @@
+package service;
+
+import java.util.HashMap;
+
+import net.sourceforge.scuba.smartcards.IResponseAPDU;
+
+/**
+ * Simple type declaration for a HashMap containing responses to protocol commands.
+ * @author wouter
+ *
+ */
+public class ProtocolResponses extends HashMap<String, IResponseAPDU>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
